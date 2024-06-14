6d394cbd3772aac1d0f145546664c71a5c5d6e33
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-06-14 15:11:58 +0900

    Add QuitSmoking app

---
+++ QuitSmoking/QuitSmoking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuitSmoking
+//
+//  Created by 한유진 on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuitSmoking/QuitSmoking/QuitSmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSmokingApp.swift
+//  QuitSmoking
+//
+//  Created by 한유진 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct QuitSmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
