ce94ec4c896a60aaf5ea6e70039297225a184534
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-06-23 18:24:19 +0900

    Add JSON web token creation

---
+++ sennight-fastapi/app/security.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from .config import settings
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=settings.ACCESS_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
