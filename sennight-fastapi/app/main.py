0e8a0c4493d83d761eee45e27a1fe070c0a083a2
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-06-23 18:43:16 +0900

    Add JSON web token validation middleware

--- sennight-fastapi/app/main.py
+++ sennight-fastapi/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from jose import jwt, JWTError
 from .database import engine, Base
 from .routes import users, factors
+from .config import settings
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +11,30 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(factors.router)
+
+
+@app.middleware("http")
+async def jwt_middleware(request: Request, call_next):
+    if request.url.path.startswith("/users"):
+        return await call_next(request)
+
+    authorization: str = request.headers.get("Authorization")
+    if authorization is None or not authorization.startswith("Bearer "):
+        return JSONResponse(
+            status_code=401,
+            content={"status": False, "detail": "Unauthorized", "data": None}
+        )
+
+    token = authorization[7:]
+    try:
+        jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except JWTError:
+        return JSONResponse(
+            status_code=401,
+            content={"status": False, "detail": "Invalid token", "data": None}
+        )
+
+    return await call_next(request)
 
 
 @app.exception_handler(HTTPException)
