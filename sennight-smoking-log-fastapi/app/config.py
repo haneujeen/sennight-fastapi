8eba88905c3d4400398cc92f02a121ff3128b02a
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-06-26 21:48:28 +0900

    Update config.py

--- sennight-smoking-log-fastapi/app/config.py
+++ sennight-smoking-log-fastapi/app/config.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:12345678@localhost:3306/sennightlogdb")
+    HOST: str = os.getenv("HOST", "localhost")
+    PORT: str = os.getenv("PORT", "8000")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "jwt_secret_key")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
 
 
 settings = Settings()
