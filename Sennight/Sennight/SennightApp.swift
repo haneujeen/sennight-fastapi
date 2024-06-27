3a98035d3a149020ee0c353e2415041374abaaf4
Eujeen Han <eujeenhan@gmail.com>
2024-06-27 11:38:44 +0900

    Add Sennight project

---
+++ Sennight/Sennight/SennightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SennightApp.swift
+//  Sennight
+//
+//  Created by 한유진 on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SennightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
