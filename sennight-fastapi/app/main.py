40a6a726d02d16a74eea7744be8e0e68baf8021a
Eujeen Han <eujeenhan@gmail.com>
2024-07-08 16:23:13 +0900

    Add mock user registration/login APIs

--- sennight-fastapi/app/main.py
+++ sennight-fastapi/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from jose import jwt, JWTError
 from .database import engine, Base
-from .routes import users, factors, motivations, milestones, symptoms, activities
+from .routes import users, mock_apis
 from .config import settings
 
 Base.metadata.create_all(bind=engine)
@@ -10,15 +10,11 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
-app.include_router(factors.router)
-app.include_router(motivations.router)
-app.include_router(milestones.router)
-app.include_router(symptoms.router)
-app.include_router(activities.router)
+app.include_router(mock_apis.router)
 
 @app.middleware("http")
 async def jwt_middleware(request: Request, call_next):
-    if request.method == "POST" and request.url.path.startswith("/users"):
+    if request.method == "POST" and request.url.path.startswith("/mock"):
         return await call_next(request)
 
     authorization: str = request.headers.get("Authorization")
