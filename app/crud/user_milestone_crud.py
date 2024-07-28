829d819a5812673dd71b5e9a05b95d2a3d2e0211
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-28 20:56:58 +0900

    Update image tag (v0.2.0)
    - GET /user-milestones/{user_id} API명 변경 (read --> get_milestones)
    - GET /user-milestones/max-id/{user_id} get_max_milestone_id API 추가

--- app/crud/user_milestone_crud.py
+++ app/crud/user_milestone_crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 from .. import models
 from ..schemas import milestone_schemas
 
@@ -11,5 +12,13 @@ def create(db: Session, user_milestone: milestone_schemas.UserMilestoneCreate):
     return db_user_milestone
 
 
-def read(db: Session, user_id: int):
+def get_milestones(db: Session, user_id: int):
     return db.query(models.UserMilestone).filter(models.UserMilestone.user_id == user_id).all()
+
+
+def get_max_milestone_id(db: Session, user_id: int):
+    return (db.query(models.UserMilestone)
+            .filter(models.UserMilestone.user_id == user_id)
+            .order_by(desc(models.UserMilestone.milestone_id))
+            .first()
+            .milestone_id)
